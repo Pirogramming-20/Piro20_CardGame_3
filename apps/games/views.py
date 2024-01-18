66a7a5f15694e06bab78471a514557febdd343f1
LeeJinHyeok <kmer1024@hanyang.ac.kr>
2024-01-18 20:22:31 +0900

    game 테스트용 user 만들기, model 수정

--- apps/games/views.py
+++ apps/games/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def game_list (request):
+    pass
+def game_detail (request):
+    pass
--- apps/users/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-# Create your models here.
-class User(AbstractUser):
-    name = models.CharField(max_length = 20)
-    score = models.IntegerField()
-    
