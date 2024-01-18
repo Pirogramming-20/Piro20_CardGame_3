8db58f597c96dcd4a3edd171affe811ea2f98f95
LeeJinHyeok <kmer1024@hanyang.ac.kr>
2024-01-19 00:58:48 +0900

    user랑 합치기 완료

--- apps/games/models.py
+++ apps/games/models.py
@@ -1,11 +1,7 @@
 from django.db import models
+from apps.users.models import User
 import random
 
-class User(models.Model):
-    name = models.CharField(max_length=20)
-    score = models.IntegerField(default=0)
-    def __str__ (self):
-        return self.name
     
 class Game(models.Model):
 
