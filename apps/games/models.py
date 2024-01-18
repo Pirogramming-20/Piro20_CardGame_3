2b25a2d5695f3ea7a523dc48699057dd7c86acbc
Leeseungchan <wkdvnddl050701@gmail.com>
2024-01-18 16:22:45 +0900

    프렉젝트 초안

---
+++ apps/games/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+# Create your models here.
+class Game:
+    user_1_NUMS_CHOICE = [
+        # ramdom 5개  
+    ]
+    user_1_NUMS_CHOICE = [
+        # ramdom 5개
+    ]
+    # user_1 = 
+    # user_2 = 
+    # user_1_card_num = 
+    # user_2_card_num =
+    # winner ??????
+    # loser ?????
