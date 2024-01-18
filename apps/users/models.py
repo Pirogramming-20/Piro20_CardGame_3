f43cb4232da0b78d18c34162a0ccb6a444915352
Github <jjyyhh0208@korea.ac.kr>
2024-01-18 22:23:44 +0900

    로그인, 회원가입 구현

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
 class User(AbstractUser):
     name = models.CharField(max_length = 20)
-    score = models.IntegerField()
-    
+    score = models.IntegerField(default = 0)
