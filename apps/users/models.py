05e4771910d05574f83d7360635b8ca57c72c67a
cathy <cathy02473500@gmail.com>
2024-01-19 12:42:43 +0900

    유저 업데이트, 디테일 페이지  css 추가

--- apps/users/models.py
+++ apps/users/models.py
@@ -6,14 +6,3 @@ class User(AbstractUser):
     # name = 본인 실명
     name = models.CharField(max_length = 20, blank=False)
     score = models.IntegerField(default = 0)
-
-# from allauth.account.signals import user_signed_up
-# from django.dispatch import receiver
-
-# @receiver(user_signed_up)
-# def set_user_name(request, user, **kwargs):
-#     # Google 계정의 이름을 가져와서 User 모델의 name 필드에 저장합니다.
-#     # Google 계정의 이름은 'first_name' 'last_name' 형태입니다.
-#     user.name = f'{user.first_name} {user.last_name}'
-#     user.save()
-
