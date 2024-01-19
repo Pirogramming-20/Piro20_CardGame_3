66cbe755739adc5907273da93f14cf415449514e
LeeJinHyeok <kmer1024@hanyang.ac.kr>
2024-01-19 13:07:53 +0900

    game 단계별로 구분

--- apps/games/urls.py
+++ apps/games/urls.py
@@ -6,6 +6,8 @@ app_name = 'games'
 urlpatterns = [
     path('list/', game_list, name='list'),
     path('detail/<int:pk>', game_detail, name='detail'),
-    path('create/', game_create, name='create'),
+    path('attack/', game_attack, name='attack'),
     path('delete/<int:pk>', game_delete, name='delete'),
+    path('match/<int:pk>', game_match, name='match'),
+    path('accept/<int:pk>', game_accept, name='accept')
 ]
--- apps/games/views.py
+++ apps/games/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from .models import Game, User
 from .forms import AttackForm
 from django.db.models import Q
+from django.http import HttpResponse
+
+import random
 # Create your views here.
 
 def game_detail (request, pk):
@@ -10,59 +13,77 @@ def game_detail (request, pk):
         ctx = {
             'game': game
         }
-        return render (request, 'games/games_detailmatch.html', ctx)
-    
+        return render (request, 'games/games_detail_match.html', ctx)
+    else:
+        return HttpResponse("Game is not yet matched.")
 
-def game_create (request):
+def game_attack(request):
     if request.user.is_authenticated:
         if request.method == "GET":
-            form = AttackForm()
+            game = Game()
+            game.user_1 = request.user
+            form = AttackForm(request=request, instance=game)  # 폼에 기존 게임 정보를 넘겨줍니다.
             ctx = {
                 "form": form
             }
-            return render (request, 'games/games_create.html', ctx)
-        
-        form = AttackForm(request.POST)
-        if form.is_valid():
-            game_instance = form.save(commit=False)
-            if game_instance.rule == 'bigger':
-                if (game_instance.user_1_card_num > game_instance.user_2_card_num):
-                    game_instance.user_2.score -= game_instance.user_2_card_num
-                    game_instance.user_1.save()
-                    game_instance.user_2.save()
-                    game_instance.winner = game_instance.user_1
-                    game_instance.loser = game_instance.user_2
-                else:
-                    game_instance.user_1.score -= game_instance.user_1_card_num
-                    game_instance.user_1.save()
-                    game_instance.user_2.save()
-                    game_instance.winner = game_instance.user_2
-                    game_instance.loser = game_instance.user_1
-                    
-            else:
-                if (game_instance.user_1_card_num < game_instance.user_2_card_num):
-                    game_instance.user_2.score -= game_instance.user_2_card_num
-                    game_instance.user_1.save()
-                    game_instance.user_2.save()
-                    game_instance.winner = game_instance.user_1
-                    game_instance.loser = game_instance.user_2  
-                else:
-                    game_instance.user_1.score -= game_instance.user_1_card_num
-                    game_instance.user_1.save()
-                    game_instance.user_2.save()
-                    game_instance.winner = game_instance.user_2
-                    game_instance.loser = game_instance.user_1
-        
-            game_instance.save()
-            game_pk = game_instance.pk
-            return redirect ('games:detail', game_pk)
-        else:
+            return render(request, 'games/games_attack.html', ctx)
+        elif request.method == "POST":
+            game = Game()
+            game.user_1 = request.user
+            form = AttackForm(request.POST, instance=game)
+            if form.is_valid():
+                form.save()
             return redirect ('games:list')
     else:
-        return redirect ('users:login')
+        return redirect('users:login')
 
+def game_match (request, pk):
+    game = Game.objects.get (id = pk)
+    if game.user_1_card_num == game.user_2_card_num:
+        game.winner = None
+        game.loser = None
+    if game.rule == 'bigger':
+        if (game.user_1_card_num > game.user_2_card_num):
+            game.user_2.score -= game.user_2_card_num
+            game.user_1.save()
+            game.user_2.save()
+            game.winner = game.user_1
+            game.loser = game.user_2
+        else:
+            game.user_1.score -= game.user_1_card_num
+            game.user_1.save()
+            game.user_2.save()
+            game.winner = game.user_2
+            game.loser = game.user_1
+                    
+    else:
+        if (game.user_1_card_num < game.user_2_card_num):
+            game.user_2.score -= game.user_2_card_num
+            game.user_1.save()
+            game.user_2.save()
+            game.winner = game.user_1
+            game.loser = game.user_2  
+        else:
+            game.user_1.score -= game.user_1_card_num
+            game.user_1.save()
+            game.user_2.save()
+            game.winner = game.user_2
+            game.loser = game.user_1
+    game.save()
+    game_pk = game.pk
+    return redirect ('games:detail', game_pk)
 
-# Create your views here.
+def game_accept (request, pk):
+    game = Game.objects.get (id = pk)
+    if request.user.is_authenticated:
+        if request.user == game.user_2:
+            if game.status == False:          
+                game.status = True
+                game.user_2_card_num = random.randint(1,11)
+                game.save()
+                return redirect ('games:match', pk)
+        
+        
 def game_list (request):
     now_user = User.objects.get(id=1)
     games = Game.objects.filter(Q(user_1=now_user) | Q(user_2=now_user))
