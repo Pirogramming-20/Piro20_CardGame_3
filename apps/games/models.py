ab830278db94efc1cee4fd3559fefb93c976875d
LeeJinHyeok <kmer1024@hanyang.ac.kr>
2024-01-19 15:22:20 +0900

    유저 input 수정

--- apps/games/models.py
+++ apps/games/models.py
@@ -4,18 +4,25 @@ import random
 
 class Game(models.Model):
 
-    user_1_card_num = models.IntegerField(default = 0)
-    user_2_card_num = models.IntegerField(default = 0)
+    AVAILABLE_NUMBERS = sorted(random.sample(range(1, 11), 5))
+
+    user_1_card_num = models.IntegerField(default = 0, choices=((num, str(num)) for num in AVAILABLE_NUMBERS))
+    user_2_card_num = models.IntegerField(default = 0, choices=((num, str(num)) for num in AVAILABLE_NUMBERS))
     
     user_1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='attackers')
     user_2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='defenders')
+    
     # 랜덤으로 룰 지정
     RULE_CHOICES = [
         ('bigger', '큰 숫자가 이기는 룰'),
         ('smaller', '작은 숫자가 이기는 룰'),
     ]
-    status = models.BooleanField(default = False)
-    rule = models.CharField(max_length=10, choices=RULE_CHOICES, default=random.choice(RULE_CHOICES))
+
+    def random_rule():
+        return random.choice(Game.RULE_CHOICES)
+
+    status = models.BooleanField(default=False)
+    rule = models.CharField(max_length=10, choices=RULE_CHOICES, default=random_rule)
     
-    winner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank = True, related_name='winner_games')
-    loser = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank = True, related_name='loser_games')
+    winner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name='winner_games')
+    loser = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name='loser_games')
