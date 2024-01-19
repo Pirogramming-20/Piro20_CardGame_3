a6581396cc798cb4c0c4ac94a96da87d96c6a5db
Leeseungchan <wkdvnddl050701@gmail.com>
2024-01-19 16:24:42 +0900

    css추가

--- CardGame/settings.py
+++ CardGame/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.games',
+    'apps.users',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
